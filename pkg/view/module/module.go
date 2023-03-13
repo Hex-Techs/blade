5ab743c264fd9562853b7da47c32cd28c057dbf8
tianzuoguo <tianzuoguo@tencent.com>
2023-03-13 19:51:43 +0800

    模块在删除时级联删除以其为父模块的所有子模块；只能修改模块的描述

--- pkg/view/module/module.go
+++ pkg/view/module/module.go
@@ -36,14 +36,14 @@ func (*ModuleController) Name() string {
 }
 
 // Create 创建新模块
-func (uc *ModuleController) Create(c *gin.Context) {
+func (mc *ModuleController) Create(c *gin.Context) {
 	var module models.Module
 	if err := c.ShouldBindJSON(&module); err != nil {
 		c.JSON(http.StatusBadRequest, web.ExceptResponse(errorMap[ErrInvalidParam], err))
 		return
 	}
 	log.Debugf("create module: %v", module)
-	if err := uc.Store.Create(context.TODO(), &module); err != nil {
+	if err := mc.Store.Create(context.TODO(), &module); err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrCreateModuleFailed], err))
 		return
 	}
@@ -51,45 +51,73 @@ func (uc *ModuleController) Create(c *gin.Context) {
 }
 
 // Delete 删除模块
-func (uc *ModuleController) Delete(c *gin.Context) {
+func (mc *ModuleController) Delete(c *gin.Context) {
 	id, err := view.GetID(c)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, web.ExceptResponse(errorMap[ErrID], err))
 		return
 	}
-	if err := uc.Store.Delete(context.TODO(), id, "", &models.Module{}); err != nil {
+	if err := mc.delete(id); err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrDeleteModuleFailed], err))
 		return
 	}
 	c.JSON(http.StatusOK, web.OkResponse())
 }
 
+// 递归删除parent_id为id的所有模块
+func (mc *ModuleController) delete(id uint) error {
+	var modules []models.Module
+	_, err := mc.Store.List(context.TODO(), -1, 1, "parent_id = "+fmt.Sprint(id), &modules)
+	if err != nil {
+		return err
+	}
+	for _, module := range modules {
+		if err := mc.delete(module.ID); err != nil {
+			return err
+		}
+	}
+	return mc.Store.Delete(context.TODO(), id, "", &models.Module{})
+}
+
 // Update 更新模块信息
-func (uc *ModuleController) Update(c *gin.Context) {
+func (mc *ModuleController) Update(c *gin.Context) {
 	id, err := view.GetID(c)
 	if err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrID], err))
 		return
 	}
-	var module models.Module
-	c.ShouldBindJSON(&module)
-	log.Debugf("update module: %v", module)
-	if err := uc.Store.Update(context.TODO(), id, "", &models.Module{}, &module); err != nil {
+	var (
+		new models.Module
+		old models.Module
+	)
+	c.ShouldBindJSON(&new)
+	if err := mc.Store.Get(context.TODO(), id, "", &old); err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrUpdateModuleFailed], err))
 		return
 	}
-	c.JSON(http.StatusOK, web.DataResponse(module))
+	if old.Description == new.Description {
+		log.Debugf("module %d description not changed", id)
+		c.JSON(http.StatusOK, web.OkResponse())
+		return
+	}
+	log.Debugw("update module", "old", old.Description, "new", new.Description)
+	old.Description = new.Description
+	if err := mc.Store.Update(context.TODO(), id, "", &models.Module{}, &old); err != nil {
+		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrUpdateModuleFailed], err))
+		return
+	}
+	c.JSON(http.StatusOK, web.OkResponse())
 }
 
 // Get 获取模块详情
-func (uc *ModuleController) Get(c *gin.Context) {
+func (mc *ModuleController) Get(c *gin.Context) {
 	id, err := view.GetID(c)
 	if err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrID], err))
 		return
 	}
 	var module models.Module
-	if err := uc.Store.Get(context.TODO(), id, "", &module); err != nil {
+	if err := mc.Store.Get(context.TODO(), id, "", &module); err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrGetModuleFailed], err))
 		return
 	}
@@ -97,18 +125,28 @@ func (uc *ModuleController) Get(c *gin.Context) {
 }
 
 // List 获取模块列表，可根据父模块id和level进行过滤
-func (uc *ModuleController) List(c *gin.Context) {
+func (mc *ModuleController) List(c *gin.Context) {
 	var req web.Request
 	c.ShouldBindQuery(&req)
 	req.Default()
 	log.Debugf("list modules: %+v", req)
+	if req.Level == 0 && req.ParentID == 0 {
+		c.JSON(http.StatusBadRequest, web.ExceptResponse(errorMap[ErrInvalidParam],
+			"level and parent_id can't be 0 at the same time"))
+		return
+	}
 	var condition string
+	if req.Level != 0 {
+		condition = "level = " + fmt.Sprint(req.Level)
+		log.Debugw("list modules by level", "condition", condition)
+	}
+	// 查询条件中，parent_id比level有更高的优先级
 	if req.ParentID != 0 {
 		condition = "parent_id = " + fmt.Sprint(req.ParentID)
 		log.Debugw("list modules by parent_id", "condition", condition)
 	}
 	var modules []models.Module
-	total, err := uc.Store.List(context.TODO(), req.Limit, req.Page, condition, &modules)
+	total, err := mc.Store.List(context.TODO(), req.Limit, req.Page, condition, &modules)
 	if err != nil {
 		c.JSON(http.StatusOK, web.ExceptResponse(errorMap[ErrGetModuleListFailed], err))
 		return
